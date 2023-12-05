603002941aea8c4fe9a6e33aaf600fb52f22e756
Abhishek Luthra <abhishek.luthra@uber.com>
2023-12-05 11:41:35 +0530

    added future builder

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,29 +15,24 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   var _groceryItems = [];
-  var _isLoading = true;
+  late Future<List<GroceryItem>> _loadedItems;
   var _error = null;
   @override
   void initState() {
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void _loadItems() async {
+  Future<List<GroceryItem>> _loadItems() async {
     final url = Uri.https(
         'flutter-prep-7582a-default-rtdb.firebaseio.com', 'shopping-list.json');
-    try{
       final response = await http.get(url);
       if(response.statusCode >= 400){
-        setState(() {
-          _error = 'Failed  to fetch data. Please try again later';
-        });
+        throw Exception('Failed  to fetch data. Please try again later');
       }
       if(response.body == 'null'){
-        setState(() {
-          _isLoading = false;
-        });
-        return;
+
+        return [];
       }
       print(response.body);
       final Map<String, dynamic> listData = json.decode(response.body);
@@ -53,16 +48,8 @@ class _GroceryListState extends State<GroceryList> {
             quantity: item.value['quantity'],
             category: category));
       }
-      setState(() {
-        _groceryItems = loadedItems;
-        _isLoading = false;
-      });
-    }
-    catch(error){
-      setState(() {
-        _error = 'Something went wrong! Please try again later';
-      });
-    }
+      return loadedItems;
+
 
   }
 
@@ -105,42 +92,41 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(
-      child: Text('No items added yet.'),
-    );
-    if(_isLoading){
-      content = const Center(child: CircularProgressIndicator(),);
-    }
-    if (_groceryItems.isNotEmpty) {
-      content = ListView.builder(
-          itemCount: _groceryItems.length,
-          itemBuilder: (ctx, index) => Dismissible(
-                key: ValueKey(_groceryItems[index].id),
-                onDismissed: (direction) {
-                  removeItem(_groceryItems[index]);
-                },
-                child: ListTile(
-                  title: Text(_groceryItems[index].name),
-                  leading: Container(
-                    width: 24,
-                    height: 24,
-                    color: _groceryItems[index].category.color,
-                  ),
-                  trailing: Text(_groceryItems[index].quantity.toString()),
-                ),
-              ));
-    }
-
-    if(_error != null){
-      content = Center(child: Text(_error),);
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: addItem, icon: const Icon(Icons.add))],
       ),
-      body: content,
+      body: FutureBuilder(future: _loadedItems, builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(child: CircularProgressIndicator(),);
+          }
+          if(snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()),);
+          }
+          if(snapshot.data!.isEmpty){
+            return const Text('No items added yet.');
+          }
+
+          List<GroceryItem> localGroceryItemList = snapshot.data!;
+          return ListView.builder(
+              itemCount: localGroceryItemList.length,
+              itemBuilder: (ctx, index) => Dismissible(
+                key: ValueKey(localGroceryItemList[index].id),
+                onDismissed: (direction) {
+                  removeItem(localGroceryItemList[index]);
+                },
+                child: ListTile(
+                  title: Text(localGroceryItemList[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: localGroceryItemList[index].category.color,
+                  ),
+                  trailing: Text(localGroceryItemList[index].quantity.toString()),
+                ),
+              ));
+      },),
     );
   }
 }
